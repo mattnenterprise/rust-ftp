45c7dafa3a07999dc38d76910563ff9a90dbd880
Zack Mullaly <zsck@riseup.net>
2016-07-25 23:14:05 -0400

    Simplified example

--- examples/connecting.rs
+++ examples/connecting.rs
@@ -1,35 +1,31 @@
 extern crate ftp;
 
 use std::str;
-use std::io::{Cursor, Error, ErrorKind, Result};
-use ftp::FtpStream;
+use std::io::Cursor;
+use ftp::{FtpStream, FtpError};
 
-fn test_ftp(addr: &str, user: &str, pass: &str) -> Result<()> {
-    let mut ftp_stream = try!(FtpStream::connect((addr, 21)));
-    try!(ftp_stream.login(user, pass));
-    println!("current dir: {}", try!(ftp_stream.pwd()));
+fn test_ftp(addr: &str, user: &str, pass: &str) -> Result<(), FtpError> {
+    let mut ftp_stream = FtpStream::connect((addr, 21)).unwrap();
+    ftp_stream.login(user, pass).unwrap();
+    println!("current dir: {}", ftp_stream.pwd().unwrap());
 
-    try!(ftp_stream.cwd("test_data"));
+    ftp_stream.cwd("test_data").unwrap();
 
     // An easy way to retrieve a file
-    let cursor = try!(ftp_stream.simple_retr("ftpext-charter.txt"));
+    let cursor = ftp_stream.simple_retr("ftpext-charter.txt").unwrap();
     let vec = cursor.into_inner();
-    let text = try!(str::from_utf8(&vec).or_else(|cause|
-        Err(Error::new(ErrorKind::Other, cause))
-    ));
+    let text = str::from_utf8(&vec).unwrap();
     println!("got data: {}", text);
 
     // Store a file
     let file_data = format!("Some awesome file data man!!");
     let mut reader = Cursor::new(file_data.into_bytes());
-    try!(ftp_stream.put("my_random_file.txt", &mut reader));
+    ftp_stream.put("my_random_file.txt", &mut reader).unwrap();
 
     ftp_stream.quit()
 }
 
 fn main() {
-    test_ftp("127.0.0.1", "Doe", "mumble").unwrap_or_else(|err|
-        panic!("{}", err)
-    );
+    test_ftp("127.0.0.1", "username", "password").unwrap();
     println!("test successful")
 }
