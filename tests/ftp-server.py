93b9b3ab93a6b37681f5e7aa9c942bcd35c2078c
Zack Mullaly <zsck@riseup.net>
2017-01-03 13:44:39 -0500

    A tiny bit of cleanup

--- tests/ftp-server.py
+++ tests/ftp-server.py
@@ -1,6 +1,6 @@
 import os
 
-from pyftpdlib.authorizers import DummyAuthorizer#, AuthenticationFailed
+from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import TLS_FTPHandler
 from pyftpdlib.servers import FTPServer
 from pyftpdlib.filesystems import AbstractedFS
@@ -14,11 +14,7 @@ handler.certfile = './test.crt'
 handler.authorizer = authorizer
 handler.passive_ports = range(60000, 65535)
 
-# Specify a masquerade address and the range of ports to use for
-# passive connections.  Decomment in case you're behind a NAT.
-#handler.masquerade_address = '151.25.42.11'
-
-# Instantiate FTP server class and listen on 0.0.0.0:2122
+# Instantiate FTP server class and listen on 0.0.0.0:21
 address = ('', 21)
 server = FTPServer(address, handler)
 
