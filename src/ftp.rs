a6a14c73581fbf07cbcd70b483859ae0487e8997
Urs Schulz <github@ursschulz.de>
2019-01-02 10:52:59 +0100

    fix doc typos

--- src/ftp.rs
+++ src/ftp.rs
@@ -38,7 +38,7 @@ pub struct FtpStream {
 }
 
 impl FtpStream {
-    /// Creates an FTP Stream.
+    /// Creates a FTP Stream.
     #[cfg(not(feature = "secure"))]
     pub fn connect<A: ToSocketAddrs>(addr: A) -> Result<FtpStream> {
         TcpStream::connect(addr)
@@ -52,7 +52,7 @@ impl FtpStream {
             })
     }
     
-    /// Creates an FTP Stream.
+    /// Creates a FTP Stream.
     #[cfg(feature = "secure")]
     pub fn connect<A: ToSocketAddrs>(addr: A) -> Result<FtpStream> {
         TcpStream::connect(addr)
